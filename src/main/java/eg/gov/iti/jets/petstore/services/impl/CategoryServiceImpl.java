ebc0996966732182e47cb1a22bc68309ed1c38f7
MAK <76202461+MahmoudAliKhalil@users.noreply.github.com>
2021-06-22 20:14:30 +0200

    Fixed package name.

--- src/main/java/eg/gov/iti/jets/petstore/services/impl/CategoryServiceImpl.java
+++ src/main/java/eg/gov/iti/jets/petstore/services/impl/CategoryServiceImpl.java
@@ -1,4 +1,4 @@
-package eg.gov.iti.jets.petstore.services.Impl;
+package eg.gov.iti.jets.petstore.services.impl;
 
 import eg.gov.iti.jets.petstore.dto.CategoryDTO;
 import eg.gov.iti.jets.petstore.dto.OrderDTO;
